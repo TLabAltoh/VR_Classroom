e5498e07cce364746ab69bc9a4b0d69544819467
TLabAltoh <TLabAltoh@gmail.com>
2023-04-18 14:41:22 +0900

    TLabHandのGripとReleaseの判定が完成

--- Assets/Tetu/VRGrabber/TLabVRGrabbable.cs
+++ Assets/Tetu/VRGrabber/TLabVRGrabbable.cs
@@ -4,13 +4,72 @@ using UnityEngine;
 
 public class TLabVRGrabbable : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public const int PARENT_LENGTH = 2;
+
+    public bool m_useRigidbody = true;
+
+    private GameObject m_mainParent;
+    private GameObject m_subParent;
+
+
+    public bool AddParent(GameObject parent)
+    {
+        if (m_mainParent == null)
+        {
+            m_mainParent = parent;
+            Debug.Log("tlabvrhand:" + parent.ToString() + " mainParent added");
+            return true;
+        }
+        else if(m_subParent == null)
+        {
+            m_subParent = parent;
+            Debug.Log("tlabvrhand:" + parent.ToString() + " subParent added");
+            return true;
+        }
+
+        Debug.Log("cannot add parent");
+        return false;
+    }
+
+    public bool RemoveParent(GameObject parent)
+    {
+        if(m_mainParent == parent)
+        {
+            if(m_subParent != null)
+            {
+                m_mainParent = m_subParent;
+                m_subParent = null;
+
+                Debug.Log("tlabvrhand:" + "m_main released and m_sub added");
+
+                return true;
+            }
+            else
+            {
+                m_mainParent = null;
+
+                Debug.Log("tlabvrhand:" + "m_main released");
+
+                return true;
+            }
+        }
+        else if(m_subParent == parent)
+        {
+            m_subParent = null;
+
+            Debug.Log("m_sub released");
+
+            return true;
+        }
+
+        return false;
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
--- Assets/Tetu/VRGrabber/TLabVRHand.cs
+++ Assets/Tetu/VRGrabber/TLabVRHand.cs
@@ -4,15 +4,77 @@ using UnityEngine;
 
 public class TLabVRHand : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public OVRInput.Controller m_controller;
+    public OVRInput.Axis1D m_grip;
+    public float m_maxDistance = 10.0f;
+    public LayerMask m_layerMask;
+
+    private Transform m_anchor;
+    private OVRCameraRig m_cameraRig;
+    private TLabVRGrabbable m_grabbable;
+    private bool m_handInitialized = false;
+
     void Start()
     {
-        
+        m_handInitialized = true;
+        m_cameraRig = FindObjectOfType<OVRCameraRig>();
+
+        if(m_controller == OVRInput.Controller.RTouch)
+        {
+            m_anchor = m_cameraRig.rightHandAnchor;
+        }
+        else if(m_controller == OVRInput.Controller.LTouch)
+        {
+            m_anchor = m_cameraRig.leftHandAnchor;
+        }
+        else
+        {
+            m_handInitialized = false;
+            Debug.LogError("The controller type is not properly selected. Select RTouch or LTouch.");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(m_handInitialized == false)
+        {
+            return;
+        }
+
+        if (m_grabbable)
+        {
+            bool grip = OVRInput.Get(m_grip, m_controller) > 0.5f;
+            if (grip == false)
+            {
+                m_grabbable.RemoveParent(this.gameObject);
+
+                m_grabbable = null;
+            }
+        }
+        else
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(m_anchor.position, m_anchor.forward);
+
+            if (Physics.Raycast(ray, out hit, m_maxDistance, m_layerMask))
+            {
+                GameObject target = hit.collider.gameObject;
+
+                bool grip = OVRInput.Get(m_grip, m_controller) > 0.5f;
+                if (grip)
+                {
+                    TLabVRGrabbable grabbable = target.GetComponent<TLabVRGrabbable>();
+
+                    if (grabbable == null)
+                    {
+                        return;
+                    }
+
+                    grabbable.AddParent(this.gameObject);
+
+                    m_grabbable = grabbable;
+                }
+            }
+        }
     }
 }
