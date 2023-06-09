a546ef3af10c39d973205bf02cbb9f175c8b1795
aho-cloud <IwaiWataru@outlook.jp>
2023-06-09 17:14:20 +0900

    C:\Users\nao\Desktop\wataru\VR_Kensyu\Assets\TLab\TLabVRGrabber

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -7,12 +7,12 @@ public class TextController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.localScale = new Vector3(-1, 1, 1);
+        //transform.localScale = new Vector3(-1, 1, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Camera.main.transform);
+        transform.LookAt(Camera.main.transform,Vector3.up);
     }
 }
