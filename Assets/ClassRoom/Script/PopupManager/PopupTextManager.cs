ebb81aab1e537c65fd53c5245120cb1e14882c07
TLabAltoh <TLabAltoh@gmail.com>
2023-06-20 14:35:07 +0900

    fix 100Mb error

--- Assets/ClassRoom/Script/PopupManager/PopupTextManager.cs
+++ Assets/ClassRoom/Script/PopupManager/PopupTextManager.cs
@@ -7,8 +7,7 @@ public class PopupTextManager : MonoBehaviour
 
     private void OnDestroy()
     {
-        if (m_controller == null) return;
-        Destroy(m_controller.gameObject);
-        Destroy(m_controller1.gameObject);
+        if (m_controller == null)   Destroy(m_controller.gameObject);
+        if (m_controller1 == null)  Destroy(m_controller1.gameObject);
     }
 }
