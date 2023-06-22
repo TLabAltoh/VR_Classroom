5c3821c1fb18ad9cdba7f348ceeee99559b954e5
TLabAltoh <121733943+TLabAltoh@users.noreply.github.com>
2023-06-23 02:01:34 +0900

    update script

--- Assets/ClassRoom/Script/Shelf/TLabShelfManager.cs
+++ Assets/ClassRoom/Script/Shelf/TLabShelfManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class TLabShelfManager : MonoBehaviour
 {
@@ -117,3 +120,40 @@ public class TLabShelfObjInfo
     public GameObject obj;
     [System.NonSerialized] public Dictionary<int, GameObject> instanced = new Dictionary<int, GameObject>();
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(TLabShelfManager))]
+[CanEditMultipleObjects]
+public class TLabShelfManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        serializedObject.Update();
+
+        TLabShelfManager manager = target as TLabShelfManager;
+
+        if (GUILayout.Button("Initialize Shelf Obj"))
+        {
+            foreach (TLabShelfObjInfo shelfInfo in manager.m_shelfObjInfos)
+            {
+                TLabVRGrabbable grabbable = shelfInfo.obj.GetComponent<TLabVRGrabbable>();
+                if (grabbable == null)
+                    grabbable = shelfInfo.obj.AddComponent<TLabVRGrabbable>();
+
+                grabbable.UseRigidbody(false, false);
+
+                TLabVRRotatable rotatable = grabbable.gameObject.GetComponent<TLabVRRotatable>();
+                if (rotatable == null)
+                    grabbable.gameObject.AddComponent<TLabVRRotatable>();
+
+                EditorUtility.SetDirty(grabbable);
+                EditorUtility.SetDirty(rotatable);
+            }
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+#endif
