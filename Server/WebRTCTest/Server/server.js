c007e95d8e6002a655106c3fef72909ad17018eb
TLabAltoh <121733943+TLabAltoh@users.noreply.github.com>
2023-07-03 11:25:09 +0900

    webrtc test

--- Server/WebRTCTest/Server/server.js
+++ Server/WebRTCTest/Server/server.js
@@ -2,15 +2,33 @@ const ws        = require('ws').Server;
 const port      = 3001;
 const server    = new ws({ port: port });
 
+var joinDic     = { };
+const OFFER     = 0;
+const ANSWER    = 1;
+const ICE       = 2;
+const JOIN      = 3;
+
 console.log('signaling server start. port=' + port);
 
-server.on('connection', function (ws) {
+server.on('connection', function (socket) {
     console.log('[server.onconnection] client connected');
 
-    ws.on('message', function (message) {
-        console.log("[ws.onmessage] " + message)
-        ws.clients.forEach(function each(client) {
-            if (ws === client) client.send(message);
-        });
+    socket.on('message', function (message) {
+        var obj = JSON.parse(message);
+        console.log("[socket.onmessage] " + "action: " + obj.action + " from: " + obj.src + " dst: " + obj.dst);
+
+        if (obj.action === JOIN) {
+            // Broadcast
+            var joinValues = Object.values(joinDic);
+            joinValues.forEach((value) => {
+                console.log("[socket.onmessage] send join message");
+                value.send(message);
+            });
+            // Add
+            joinDic[obj.src] = socket;
+        } else if (obj.action === ICE || obj.action === OFFER || obj.action === ANSWER) {
+            // Unicast
+            joinDic[obj.dst].send(message);
+        }
     });
 });
