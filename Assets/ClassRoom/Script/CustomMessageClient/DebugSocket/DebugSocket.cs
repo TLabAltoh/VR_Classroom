acc79bca6958544b71fa5d8222812c706ef9afec
TLabAltoh <TLabAltoh@gmail.com>
2023-12-04 18:16:02 +0900

    update demo scene

--- Assets/ClassRoom/Script/CustomMessageClient/DebugSocket/DebugSocket.cs
+++ Assets/ClassRoom/Script/CustomMessageClient/DebugSocket/DebugSocket.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using TLab.XR.Network;
 using TLab.XR.Interact;
@@ -16,6 +17,7 @@ namespace TLab.VRClassroom
         public WebVector3 position = null;
         public WebVector4 rotation = null;
         public int grabbIndex = ExclusiveController.FREE;
+        public string frame = null;
     }
 
     public enum WebDebugAction
@@ -46,7 +48,23 @@ namespace TLab.VRClassroom
 
         [SerializeField] private TextMeshProUGUI m_grabbIndex;
 
+        [Header("Capture Settings")]
+
+        [SerializeField] private Camera m_camera;
+        [SerializeField] private Vector2Int m_resolution = new Vector2Int(DEFAULT_RES, DEFAULT_RES);
+
+        [Header("Display Settings")]
+
+        [SerializeField] private RawImage m_display;
+
+        private Texture2D m_tex;
+
+        private Texture2D m_copyTex;
+        private RenderTexture m_renderTex;
+
         private string THIS_NAME => "[" + this.GetType().Name + "] ";
+
+        private const int DEFAULT_RES = 128;
 
         public const int BROADCAST = -1;
 
@@ -55,6 +73,8 @@ namespace TLab.VRClassroom
         private const float ZERO_FRAME = 0.0f;
 
         private float m_elapsed = 0.0f;
+
+        private byte[] m_frame;
 
         private ExclusiveController m_controller;
 
@@ -72,7 +92,7 @@ namespace TLab.VRClassroom
                 seatIndex: dstIndex, customIndex: m_customIndex, custom: message);
         }
 
-        public void SendDebugActionMessage(WebDebugAction action, ExclusiveController controller = null, int dstIndex = BROADCAST)
+        public void SendDebugActionMessage(WebDebugAction action, ExclusiveController controller = null, bool frame = false, int dstIndex = BROADCAST)
         {
             var obj = new DebugSocketJson
             {
@@ -83,8 +103,14 @@ namespace TLab.VRClassroom
                 rbAllocated = controller.rbAllocated,
                 position = new WebVector3 { x = controller.transform.position.x, y = controller.transform.position.y, z = controller.transform.position.z },
                 rotation = new WebVector4 { x = controller.transform.rotation.x, y = controller.transform.rotation.y, z = controller.transform.rotation.z, w = controller.transform.rotation.w },
-                grabbIndex = controller.grabbedIndex
+                grabbIndex = controller.grabbedIndex,
+                frame = ""
             };
+
+            if (frame)
+            {
+                obj.frame = System.Convert.ToBase64String(m_frame);
+            }
 
             string json = JsonUtility.ToJson(obj);
             SendWsMessage(json, dstIndex);
@@ -124,6 +150,14 @@ namespace TLab.VRClassroom
                     m_position.text = obj.position.x.ToString("0.00") + ", " + obj.position.y.ToString("0.00") + ", " + obj.position.z.ToString("0.00");
                     m_rotation.text = obj.rotation.x.ToString("0.00") + ", " + obj.rotation.y.ToString("0.00") + ", " + obj.rotation.z.ToString("0.00") + ", " + obj.rotation.w.ToString("0.00"); 
                     m_grabbIndex.text = obj.grabbIndex.ToString();
+
+                    if (obj.frame != "")
+                    {
+                        byte[] data = System.Convert.FromBase64String(obj.frame);
+
+                        UpdateTexture(data);
+                    }
+
                     break;
             }
         }
@@ -136,18 +170,54 @@ namespace TLab.VRClassroom
 
         public void SyncState(string id) => SendDebugActionMessage(WebDebugAction.SWITCH_DEBUG_TARGET, ExclusiveController.GetById(id));
 
+        public void UpdateTexture(byte[] data)
+        {
+            m_tex.LoadRawTextureData(data);
+            m_tex.Apply();
+        }
+
+        void Start()
+        {
+            m_camera.enabled = false;
+
+            m_tex = new Texture2D(m_resolution.x, m_resolution.y, TextureFormat.ARGB32, false);
+            m_tex.Apply();
+
+            m_display.texture = m_tex;
+
+            m_copyTex = new Texture2D(m_resolution.x, m_resolution.y, TextureFormat.ARGB32, false);
+
+            m_renderTex = new RenderTexture(m_resolution.x, m_resolution.y, 16, RenderTextureFormat.ARGB32);
+
+            m_camera.targetTexture = m_renderTex;
+        }
+
         void Update()
         {
             if (m_controller != null)
             {
                 if (m_elapsed > INTERVAL)
                 {
-                    SendDebugActionMessage(WebDebugAction.SYNC_STATE, m_controller);
+                    SendDebugActionMessage(WebDebugAction.SYNC_STATE, m_controller, frame: true);
 
                     m_elapsed = ZERO_FRAME;
                 }
 
                 m_elapsed += Time.deltaTime;
+            }
+
+            if (m_camera != null)
+            {
+                RenderTexture.active = m_renderTex;
+
+                m_camera.Render();
+
+                m_copyTex.ReadPixels(new Rect(0, 0, m_copyTex.width, m_copyTex.height), 0, 0);
+                m_copyTex.Apply();
+
+                m_frame = m_copyTex.GetRawTextureData();
+
+                RenderTexture.active = null;
             }
         }
     }
